3d1b595a625ad21f9188f583251d4820d7094ba0
Bruno Moraes <brunodelciomoraes@gmail.com>
2021-10-03 19:07:01 -0300

    Estruturando o projeto front end do app

---
+++ task-organizer/src/components/home/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Organize seu dia de maneira fácil e rápida">
+        <div className='display-4'>Bem Vindo!</div>
+    </Main>
---
+++ task-organizer/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido por <strong> <span className="text-danger">B</span>runo</strong>
+            <strong> <span className="text-danger">M</span>oraes</strong>
+        </span>
+    </footer>
